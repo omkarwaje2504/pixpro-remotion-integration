3bb605f9d799f847469ec5de1bf37996797f50a4
omkar <omkar@saiashirwad.com>
2025-07-05 21:16:43 +0530

    update

--- next.config.js
+++ next.config.js
@@ -3,16 +3,10 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/",
+        source: "/ffmpeg/:path*",
         headers: [
-          {
-            key: "Cross-Origin-Embedder-Policy",
-            value: "require-corp",
-          },
-          {
-            key: "Cross-Origin-Opener-Policy",
-            value: "same-origin",
-          },
+          { key: "Cross-Origin-Embedder-Policy", value: "require-corp" },
+          { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
         ],
       },
     ];
